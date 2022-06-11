ce7cb11c42f661077b5383de28e6d8fb09284a8c
rohantare03 <rohatare123@gmail.com>
2022-06-11 19:11:58 +0530

    [Rohan] Add : Caculating Total fare and Average fare per ride

--- CabGenerator/CabTestCases/UnitTest1.cs
+++ CabGenerator/CabTestCases/UnitTest1.cs
@@ -56,5 +56,21 @@ namespace CabTestCases
             rides.Add(ride2);
             Assert.AreEqual(43.0d, invoiceGenerator.TotalFareForMultipleRideReturn(rides));
         }
+        //<summary>
+        //UC3: Enhanced the fare by finding average fare, totalfare, number of rides
+        //</summary>
+        [Test]
+        public void GivenListOfRides_GenerateInvoice()
+        {
+            Ride ride1 = new Ride(2, 2);
+            Ride ride2 = new Ride(2, 1);
+
+            List<Ride> rides = new List<Ride>();
+            rides.Add(ride1);
+            rides.Add(ride2);
+            Assert.AreEqual(43.0d, invoiceGenerator.TotalFareForMultipleRideReturn(rides));
+            Assert.AreEqual(21.5d, invoiceGenerator.averagePerRide);
+            Assert.AreEqual(2, invoiceGenerator.numOfRides);
+        }
     }
 }
